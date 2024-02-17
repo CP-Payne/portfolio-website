2b8ed2172b951b7ed694075cf3a61ccc31b58868
CP Payne <charlethical1@gmail.com>
2024-02-17 06:31:59 +0200

    Added dark mode to minimal portfolio project - project finished

--- minimal/minimal-portfolio/src/componenets/Intro.jsx
+++ minimal/minimal-portfolio/src/componenets/Intro.jsx
@@ -1,7 +1,9 @@
 const Intro = () => {
   return (
     <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
-      <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">Intro</h1>
+      <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold dark:text-white">
+        Charles Payne
+      </h1>
       <p className="text-base md:text-xl mb-3 font-medium">
         Software Engineer | Soon-To-Be-Graduate
       </p>
--- minimal/minimal-portfolio/src/componenets/PortfolioItem.jsx
+++ minimal/minimal-portfolio/src/componenets/PortfolioItem.jsx
@@ -1,27 +1,35 @@
 import PropTypes from "prop-types";
 
+// eslint-disable-next-line react/prop-types
 const PortfolioItem = ({ title, imgUrl, stack, link }) => {
   return (
-    <div className="border-2 border-stone-900 rounded-md overflow-hidden">
+    <a
+      href={link}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="border-2 border-stone-900 rounded-md overflow-hidden dark:border-white"
+    >
       <img
         src={imgUrl}
         alt="portfolio"
         className="w-full h-36 md:h-48 object-cover cursor-pointer"
       />
       <div className="w-full p-4">
-        <h3 className="text-lg md:text-xl mb-2 mb:mb-3 font-bold">{title}</h3>
-        <p className="flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm">
+        <h3 className="text-lg md:text-xl mb-2 mb:mb-3 font-bold dark:text-white">
+          {title}
+        </h3>
+        <p className="flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm dark:text-white">
           {stack.map((item, index) => (
             <span
               key={index}
-              className="inline-block px-2 py-1 font-semibold border-2 border-stone-900 rounded-md"
+              className="inline-block px-2 py-1 font-semibold border-2 border-stone-900 dark:border-white rounded-md"
             >
               {item}
             </span>
           ))}
         </p>
       </div>
-    </div>
+    </a>
   );
 };
 
