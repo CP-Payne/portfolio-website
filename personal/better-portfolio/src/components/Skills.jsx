fe1c5a6e46f28e5f00caf5cffbfe5e16eb524aca
CP Payne <charlethical1@gmail.com>
2024-04-26 14:15:23 +0200

    Skills updated
    Added and removed some skills

--- personal/better-portfolio/src/components/Skills.jsx
+++ personal/better-portfolio/src/components/Skills.jsx
@@ -3,10 +3,11 @@ import CSS from "../assets/css.png";
 import JavaScript from "../assets/javascript.png";
 import ReactImg from "../assets/react.png";
 import Node from "../assets/node.png";
-import FireBase from "../assets/firebase.png";
-import GitHub from "../assets/github.png";
 import Tailwind from "../assets/tailwind.png";
-import Mongo from "../assets/mongo.png";
+import GO from "../assets/go.png";
+import Python from "../assets/python.png";
+import Docker from "../assets/docker.png";
+import PostgreSQL from "../assets/postgress.png";
 
 const Skills = () => {
   return (
@@ -24,46 +25,76 @@ const Skills = () => {
         </div>
 
         <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 pt-6 hover:bg-mainDarkGray hover:shadow-lightGoldColor hover:shadow-lg">
-            <img className="w-20 mx-auto" src={HTML} alt="HTML icon" />
+          <div
+              className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 pt-6 hover:bg-mainDarkGray hover:shadow-lightGoldColor hover:shadow-lg">
+            <img className="w-20 mx-auto" src={HTML} alt="HTML icon"/>
             <p className="my-4">HTML</p>
           </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 pt-6 hover:bg-mainDarkGray hover:shadow-lightGoldColor hover:shadow-lg">
-            <img className="w-20 mx-auto" src={CSS} alt="HTML icon" />
+          <div
+              className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 pt-6 hover:bg-mainDarkGray hover:shadow-lightGoldColor hover:shadow-lg">
+            <img className="w-20 mx-auto" src={CSS} alt="CSS icon"/>
             <p className="my-4">CSS</p>
           </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 pt-6 hover:bg-mainDarkGray hover:shadow-lightGoldColor hover:shadow-lg">
-            <img className="w-20 mx-auto" src={JavaScript} alt="HTML icon" />
+          <div
+              className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 pt-6 hover:bg-mainDarkGray hover:shadow-lightGoldColor hover:shadow-lg">
+            <img
+                className="w-20 mx-auto"
+                src={JavaScript}
+                alt="JavaScript icon"
+            />
             <p className="my-4">JAVASCRIPT</p>
           </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 pt-6 hover:bg-mainDarkGray hover:shadow-lightGoldColor hover:shadow-lg">
-            <img className="w-20 mx-auto" src={ReactImg} alt="HTML icon" />
+          <div
+              className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 pt-6 hover:bg-mainDarkGray hover:shadow-lightGoldColor hover:shadow-lg">
+            <img
+                //className="w-20 mx-auto"
+                className="w-40 mx-auto"
+                src={GO}
+                alt="Go icon"
+            />
+            <p className="my-4">GO</p>
+          </div>
+
+          <div
+              className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 pt-6 hover:bg-mainDarkGray hover:shadow-lightGoldColor hover:shadow-lg">
+            <img
+                className="w-20 mx-auto"
+                src={Python}
+                alt="Python icon"
+            />
+            <p className="my-4">PYTHON</p>
+          </div>
+          <div
+              className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 pt-6 hover:bg-mainDarkGray hover:shadow-lightGoldColor hover:shadow-lg">
+            <img className="w-20 mx-auto" src={ReactImg} alt="React icon"/>
             <p className="my-4">REACT</p>
           </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 pt-6 hover:bg-mainDarkGray hover:shadow-lightGoldColor hover:shadow-lg">
-            <img className="w-20 mx-auto" src={GitHub} alt="HTML icon" />
-            <p className="my-4">GITHUB</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 pt-6 hover:bg-mainDarkGray hover:shadow-lightGoldColor hover:shadow-lg">
-            <img className="w-20 mx-auto" src={Node} alt="HTML icon" />
+
+          <div
+              className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 pt-6 hover:bg-mainDarkGray hover:shadow-lightGoldColor hover:shadow-lg">
+            <img className="w-20 mx-auto" src={Node} alt="Node icon"/>
             <p className="my-4">NODE JS</p>
           </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 pt-6 hover:bg-mainDarkGray hover:shadow-lightGoldColor hover:shadow-lg">
-            <img className="w-20 mx-auto" src={Mongo} alt="HTML icon" />
-            <p className="my-4">MONGO DB</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 pt-6 hover:bg-mainDarkGray hover:shadow-lightGoldColor hover:shadow-lg">
-            <img className="w-20 mx-auto" src={Tailwind} alt="HTML icon" />
+          <div
+              className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 pt-6 hover:bg-mainDarkGray hover:shadow-lightGoldColor hover:shadow-lg">
+            <img className="w-20 mx-auto" src={Tailwind} alt="Tailwind icon"/>
             <p className="my-4">TAILWIND</p>
           </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 pt-6 hover:bg-mainDarkGray hover:shadow-lightGoldColor hover:shadow-lg">
-            <img className="w-20 mx-auto" src={FireBase} alt="HTML icon" />
-            <p className="my-4">FIREBASE</p>
+          <div
+              className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 pt-6 hover:bg-mainDarkGray hover:shadow-lightGoldColor hover:shadow-lg">
+            <img className="w-20 mx-auto" src={PostgreSQL} alt="PostgreSQL icon"/>
+            <p className="my-4">POSTGRESQL</p>
+          </div>
+
+          <div
+              className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 pt-6 hover:bg-mainDarkGray hover:shadow-lightGoldColor hover:shadow-lg">
+            <img className="w-[7.5rem] mx-auto" src={Docker} alt="Docker icon"/>
+            <p className="my-4">DOCKER</p>
           </div>
         </div>
       </div>
       {/*Dummy element for padding - Self enclosing div*/}
-      <div className="max-md:pb-16" />
+      <div className="max-md:pb-16"/>
     </div>
   );
 };
