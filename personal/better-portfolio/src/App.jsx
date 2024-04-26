b6b1fa6112d445020b8910a6b9e22ab5e55ca42b
CP Payne <charlethical1@gmail.com>
2024-04-26 12:47:28 +0200

    App does not start from the top of the page on firefox browsers
    Implemented a workaround so that after a second delay after initial load the window will jump to position 0, 0 on firefox.

--- personal/better-portfolio/src/App.jsx
+++ personal/better-portfolio/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
@@ -11,81 +11,91 @@ import { scroller } from "react-scroll";
 const sections = ["home", "about", "skills", "project", "contact"];
 
 function App() {
-    const [activeSection, setActiveSection] = useState(0);
-    const [canScroll, setCanScroll] = useState(true);
+  const [activeSection, setActiveSection] = useState(0);
+  const [canScroll, setCanScroll] = useState(true);
 
-    const scrollToSection = (sectionName) => {
-        scroller.scrollTo(sectionName, {
-            duration: 800,
-            delay: 0,
-            smooth: "easeInOutQuart",
-        });
+  const scrollToSection = (sectionName) => {
+    scroller.scrollTo(sectionName, {
+      duration: 800,
+      delay: 0,
+      smooth: "easeInOutQuart",
+    });
+  };
+
+  useEffect(() => {
+    const handleScroll = (e) => {
+      if (window.innerWidth >= 743) {
+        if (!canScroll) {
+          e.preventDefault(); // Prevent default scrolling when canScroll is false
+          return; // Exit the function early if scrolling is not allowed
+        }
+      }
+
+      // Assuming canScroll is initially true, then...
+      const direction = e.deltaY > 0 ? "down" : "up";
+      console.log(direction);
+      e.preventDefault(); // Also prevent the default scroll behavior when canScroll is true
+
+      let newSection = activeSection;
+      if (direction === "down" && activeSection < sections.length - 1) {
+        newSection = activeSection + 1;
+      } else if (direction === "up" && activeSection > 0) {
+        newSection = activeSection - 1;
+      }
+
+      if (newSection !== activeSection) {
+        setActiveSection(newSection);
+        setCanScroll(false); // Disable further scrolling
+
+        setTimeout(() => {
+          setCanScroll(true); // Re-enable scrolling after 3 seconds
+        }, 100);
+      }
     };
 
-    useEffect(() => {
-        const handleScroll = (e) => {
-            if (window.innerWidth >= 743) {
-                if (!canScroll) {
-                    e.preventDefault(); // Prevent default scrolling when canScroll is false
-                    return; // Exit the function early if scrolling is not allowed
-                }
-            }
+    // Add wheel event listener with passive set to false to allow preventDefault
+    window.addEventListener("wheel", handleScroll, { passive: false });
+    return () => {
+      window.removeEventListener("wheel", handleScroll);
+    };
+  }, [activeSection, canScroll]);
 
-            // Assuming canScroll is initially true, then...
-            const direction = e.deltaY > 0 ? "down" : "up";
-            console.log(direction);
-            e.preventDefault(); // Also prevent the default scroll behavior when canScroll is true
+  useEffect(() => {
+    if (window.innerWidth >= 700) {
+      if (canScroll) {
+        // Only scroll to the section if scrolling is enabled
+        scrollToSection(sections[activeSection]);
+      }
+    }
+  }, [activeSection, canScroll]);
 
-            let newSection = activeSection;
-            if (direction === "down" && activeSection < sections.length - 1) {
-                newSection = activeSection + 1;
-            } else if (direction === "up" && activeSection > 0) {
-                newSection = activeSection - 1;
-            }
-
-            if (newSection !== activeSection) {
-                setActiveSection(newSection);
-                setCanScroll(false); // Disable further scrolling
-
-                setTimeout(() => {
-                    setCanScroll(true); // Re-enable scrolling after 3 seconds
-                }, 200);
-            }
-        };
-
-        // Add wheel event listener with passive set to false to allow preventDefault
-        window.addEventListener("wheel", handleScroll, { passive: false });
-        return () => {
-            window.removeEventListener("wheel", handleScroll);
-        };
-    }, [activeSection, canScroll]);
-
-    useEffect(() => {
-        if (window.innerWidth >= 700) {
-            if (canScroll) {
-                // Only scroll to the section if scrolling is enabled
-                scrollToSection(sections[activeSection]);
-            }
-        }
-    }, [activeSection, canScroll]);
-    return (
-        <>
-            <div>
-                <Navbar
-                    activeSection={activeSection}
-                    setActiveSection={setActiveSection}
-                />
-                <Home
-                    activeSection={activeSection}
-                    setActiveSection={setActiveSection}
-                />
-                <About />
-                <Skills />
-                <Project />
-                <Contact />
-            </div>
-        </>
-    );
+  // There seems to be a bug where if opened in firefox, the application does not start at the home page
+  useEffect(() => {
+    let isFirefox = navigator.userAgent.includes("Firefox");
+    if (isFirefox) {
+      setTimeout(() => {
+        window.scrollTo(0, 0);
+      }, 1000);
+    }
+  }, []);
+  return (
+    <>
+      <div>
+        <Navbar
+          activeSection={activeSection}
+          setActiveSection={setActiveSection}
+        />
+        <Home
+          activeSection={activeSection}
+          setActiveSection={setActiveSection}
+        />
+        <About />
+        <Skills />
+        <Project />
+        <Contact />
+      </div>
+    </>
+  );
 }
 
 export default App;
--- personal/better-portfolio/src/components/Navbar.jsx
+++ personal/better-portfolio/src/components/Navbar.jsx
@@ -7,164 +7,165 @@ import Logo from "../assets/logo-2-strong-gold-removebg.png";
 import { Link } from "react-scroll";
 
 const Navbar = ({ activeSection, setActiveSection }) => {
-    const setActiveSectionTo = (sectionId) => {
-        setActiveSection(sectionId);
-        console.log(activeSection);
-    };
-    const [nav, setNav] = useState(false);
-    const handleClick = () => setNav(!nav);
-    return (
-        <div className="fixed w-full h-[100px] max-md:h-[50px] max-md:pb-3 flex justify-between items-center px-4 bg-mainLightGray text-gray-300 ">
-            <div className="mt-3">
-                <Link to="home" smooth={true} duration={500}>
-                    <img
-                        src={Logo}
-                        alt="Logo Image"
-                        className="hover:cursor-pointer pb-0 max-md:w-[60px] w-[90px]"
-                    //style={{ width: "150px", height: "80px" }}
-                    //style={{ width: "75px", height: "40px" }}
-                    />
-                </Link>
-            </div>
-            {/* Menu */}
-            <ul className="hidden md:flex font-bold text-[1.2rem]">
-                <li>
-                    <Link
-                        to="home"
-                        smooth={true}
-                        duration={500}
-                        onClick={() => setActiveSectionTo(0)}
-                    >
-                        Home
-                    </Link>
-                </li>
-                <li>
-                    <Link
-                        to="about"
-                        smooth={true}
-                        duration={500}
-                        onClick={() => setActiveSectionTo(1)}
-                    >
-                        About
-                    </Link>
-                </li>
-                <li>
-                    <Link
-                        to="skills"
-                        smooth={true}
-                        duration={500}
-                        onClick={() => setActiveSectionTo(2)}
-                    >
-                        Skills
-                    </Link>
-                </li>
-                <li>
-                    <Link
-                        to="project"
-                        smooth={true}
-                        duration={500}
-                        onClick={() => setActiveSectionTo(3)}
-                    >
-                        Project
-                    </Link>
-                </li>
-                <li>
-                    <Link
-                        to="contact"
-                        smooth={true}
-                        duration={500}
-                        onClick={() => setActiveSectionTo(4)}
-                    >
-                        Contact
-                    </Link>
-                </li>
-            </ul>
-            {/* Hamburger*/}
-            <div onClick={handleClick} className="md:hidden z-10">
-                {!nav ? <FaBars /> : <FaTimes />}
-            </div>
-            {/*Mobile Menu*/}
-            <ul
-                className={
-                    !nav
-                        ? "hidden"
-                        : "absolute top-0 left-0 w-full h-screen bg-mainLightGray flex flex-col justify-center items-center"
-                }
+  const setActiveSectionTo = (sectionId) => {
+    setActiveSection(sectionId);
+    console.log(activeSection);
+  };
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
+
+  return (
+    <div className="fixed w-full h-[100px] max-md:h-[50px] max-md:pb-3 flex justify-between items-center px-4 bg-mainLightGray text-gray-300 ">
+      <div className="mt-3">
+        <Link to="home" smooth={true} duration={500}>
+          <img
+            src={Logo}
+            alt="Logo Image"
+            className="hover:cursor-pointer pb-0 max-md:w-[60px] w-[90px]"
+            //style={{ width: "150px", height: "80px" }}
+            //style={{ width: "75px", height: "40px" }}
+          />
+        </Link>
+      </div>
+      {/* Menu */}
+      <ul className="hidden md:flex font-bold text-[1.2rem]">
+        <li>
+          <Link
+            to="home"
+            smooth={true}
+            duration={500}
+            onClick={() => setActiveSectionTo(0)}
+          >
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="about"
+            smooth={true}
+            duration={500}
+            onClick={() => setActiveSectionTo(1)}
+          >
+            About
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="skills"
+            smooth={true}
+            duration={500}
+            onClick={() => setActiveSectionTo(2)}
+          >
+            Skills
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="project"
+            smooth={true}
+            duration={500}
+            onClick={() => setActiveSectionTo(3)}
+          >
+            Project
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="contact"
+            smooth={true}
+            duration={500}
+            onClick={() => setActiveSectionTo(4)}
+          >
+            Contact
+          </Link>
+        </li>
+      </ul>
+      {/* Hamburger*/}
+      <div onClick={handleClick} className="md:hidden z-10">
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
+      {/*Mobile Menu*/}
+      <ul
+        className={
+          !nav
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen bg-mainLightGray flex flex-col justify-center items-center"
+        }
+      >
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="home" smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          {" "}
+          <Link onClick={handleClick} to="about" smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          {" "}
+          <Link onClick={handleClick} to="skills" smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          {" "}
+          <Link onClick={handleClick} to="project" smooth={true} duration={500}>
+            Project
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          {" "}
+          <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
+      </ul>
+      {/*Social icons*/}
+      <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
+        <ul>
+          <li className="w-[80px] md:w-[142px] lg:w-[160px] h-[40px] lg:h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
+            <a
+              href="/"
+              className="flex justify-between items-center w-full text-gray-300"
             >
-                <li className="py-6 text-4xl">
-                    <Link onClick={handleClick} to="home" smooth={true} duration={500}>
-                        Home
-                    </Link>
-                </li>
-                <li className="py-6 text-4xl">
-                    {" "}
-                    <Link onClick={handleClick} to="about" smooth={true} duration={500}>
-                        About
-                    </Link>
-                </li>
-                <li className="py-6 text-4xl">
-                    {" "}
-                    <Link onClick={handleClick} to="skills" smooth={true} duration={500}>
-                        Skills
-                    </Link>
-                </li>
-                <li className="py-6 text-4xl">
-                    {" "}
-                    <Link onClick={handleClick} to="project" smooth={true} duration={500}>
-                        Project
-                    </Link>
-                </li>
-                <li className="py-6 text-4xl">
-                    {" "}
-                    <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
-                        Contact
-                    </Link>
-                </li>
-            </ul>
-            {/*Social icons*/}
-            <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
-                <ul>
-                    <li className="w-[80px] md:w-[142px] lg:w-[160px] h-[40px] lg:h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
-                        <a
-                            href="/"
-                            className="flex justify-between items-center w-full text-gray-300"
-                        >
-                            Linkedin{" "}
-                            <FaLinkedin
-                                className="mr-[-10px] lg:mr-0 duration-300"
-                                size={30}
-                            />
-                        </a>
-                    </li>{" "}
-                    <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]">
-                        <a
-                            href="/"
-                            className="flex justify-between items-center w-full text-gray-300"
-                        >
-                            Github
-                            <FaGithub size={30} />
-                        </a>
-                    </li>{" "}
-                    <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]">
-                        <a
-                            href="/"
-                            className="flex justify-between items-center w-full text-gray-300"
-                        >
-                            Email <HiOutlineMail size={30} />
-                        </a>
-                    </li>{" "}
-                    <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
-                        <a
-                            href="/"
-                            className="flex justify-between items-center w-full text-gray-300"
-                        >
-                            Resume <BsFillPersonLinesFill size={30} />
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    );
+              Linkedin{" "}
+              <FaLinkedin
+                className="mr-[-10px] lg:mr-0 duration-300"
+                size={30}
+              />
+            </a>
+          </li>{" "}
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]">
+            <a
+              href="/"
+              className="flex justify-between items-center w-full text-gray-300"
+            >
+              Github
+              <FaGithub size={30} />
+            </a>
+          </li>{" "}
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]">
+            <a
+              href="/"
+              className="flex justify-between items-center w-full text-gray-300"
+            >
+              Email <HiOutlineMail size={30} />
+            </a>
+          </li>{" "}
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
+            <a
+              href="/"
+              className="flex justify-between items-center w-full text-gray-300"
+            >
+              Resume <BsFillPersonLinesFill size={30} />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
